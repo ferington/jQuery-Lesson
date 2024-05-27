2a1a8a49d9c74dc49c86ac8b42f8c4fca3601770
ferington <155619800+ferington@users.noreply.github.com>
2024-05-27 21:56:42 +0900

    練習画像表示

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 $(function(){
-  $('.box1').slideUp(3000)({
-    'background-color' : '#0000FF',
-    'height': '100px'
-  });
+  $('.box1').show();
+  $('.box1').css({'background-color': '#0000FF'});
 });
